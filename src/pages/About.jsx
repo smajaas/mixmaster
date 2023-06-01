0cc646dd9c5a8bfc09c67ac3a78e70acd628703b
smajaas <smajaas@gmail.com>
2023-06-01 11:39:45 +0530

    about page setup

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,12 +1,18 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import Wrapper from '../assets/wrappers/AboutPage';
 
 const About = () => {
   return (
-    <div>
+    <Wrapper>
       <h1>about</h1>
-      <Link to='/'>back home</Link>
-    </div>
+      <p>
+        Introducing "MixMaster," the ultimate party sidekick app that fetches
+        cocktails from the hilarious Cocktails DB API. With a flick of your
+        finger, you'll unlock a treasure trove of enchanting drink recipes
+        that'll make your taste buds dance and your friends jump with joy. Get
+        ready to shake up your mixology game, one fantastical mocktail at a
+        time, and let the laughter and giggles flow!
+      </p>
+    </Wrapper>
   );
 };
 
