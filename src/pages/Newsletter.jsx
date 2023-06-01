6b6354f320ac2dc2f811769673746723c737ef44
smajaas <smajaas@gmail.com>
2023-06-01 22:17:58 +0530

    newsletter setup

--- src/pages/Newsletter.jsx
+++ src/pages/Newsletter.jsx
@@ -2,9 +2,57 @@ import React from 'react';
 
 const Newsletter = () => {
   return (
-    <div>
-      <h1>news letter</h1>
-    </div>
+    <form className='form'>
+      <h4 style={{ textAlign: 'center', marginBottom: '2rem' }}>
+        our newsletter
+      </h4>
+      {/* {name} */}
+      <div className='form-row'>
+        <label htmlFor='name' className='form-label'>
+          name
+        </label>
+        <input
+          type='text'
+          className='form-input'
+          name='name'
+          id='name'
+          defaultValue='talal'
+        />
+      </div>
+      {/* {lastName} */}
+      <div className='form-row'>
+        <label htmlFor='lastName' className='form-label'>
+          last name
+        </label>
+        <input
+          type='text'
+          className='form-input'
+          name='lastName'
+          id='lastName'
+          defaultValue='afaan'
+        />
+      </div>
+      {/* {email} */}
+      <div className='form-row'>
+        <label htmlFor='email' className='form-label'>
+          email
+        </label>
+        <input
+          type='text'
+          className='form-input'
+          name='email'
+          id='email'
+          defaultValue='test@test.com'
+        />
+      </div>
+      <button
+        type='submit'
+        className='btn btn-block'
+        style={{ marginTop: '0.5rem' }}
+      >
+        submit
+      </button>
+    </form>
   );
 };
 
