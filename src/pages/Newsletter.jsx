f89f5b43170e6d41bf9dd2d8ed5ecf8973a3b6d0
smajaas <smajaas@gmail.com>
2023-06-01 22:49:48 +0530

    submit form

--- src/pages/Newsletter.jsx
+++ src/pages/Newsletter.jsx
@@ -1,9 +1,15 @@
-import { Form } from 'react-router-dom';
+import { Form, redirect, useNavigation } from 'react-router-dom';
+import axios from 'axios';
+import { toast } from 'react-toastify';
+
+const newsletterUrl = 'https://www.course-api.com/cocktails-newsletter';
 
 export const action = async ({ request }) => {
   const formData = await request.formData();
   const data = Object.fromEntries(formData);
-  return null;
+  const response = await axios.post(newsletterUrl, data);
+  toast.success(response.data.msg);
+  return redirect('/');
 };
 
 const Newsletter = () => {
